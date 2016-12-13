80acef3c28539ed39ff8cc99c4a23a112899baff
karachev <pavelkarachev@ymail.com>
2016-12-13 21:47:49 +0300

    0.1.1
    Some beatu.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-void gotoxy(int x, int y)
-{
+void gotoxy(int x, int y) {
   static HANDLE h = NULL;
   if(!h)
     h = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -14,55 +13,46 @@ void gotoxy(int x, int y)
   SetConsoleCursorPosition(h,c);
 }
 
-int main()
-{
+int main() {
     int Keys;
     int poz_x = 10;
     int poz_y = 10;
     gotoxy(poz_x,poz_y);
 
-    while(true)
-    {
+    while(true) {
         Keys = getch();
-        if (Keys == 77)
-        {
-
-                poz_x+=1;
-                gotoxy(poz_x,poz_y);
-                cout << char(42);
-                gotoxy(poz_x-1,poz_y);
-                cout << " ";
+        if (Keys == 77) {
+            poz_x+=1;
+            gotoxy(poz_x,poz_y);
+            cout << char(42);
+            gotoxy(poz_x-1,poz_y);
+            cout << " ";
 
         }
-        else if (Keys == 75)
-        {
 
+        else if (Keys == 75) {
             poz_x-=1;
             gotoxy(poz_x,poz_y); //72 vniz 80 verh
             cout << char(42);
             gotoxy(poz_x+1,poz_y);
             cout << " ";
+        }
 
-
-                    }
-        else if (Keys == 72 )
-        {
+        else if (Keys == 72 ) {
             poz_y-=1;
-                    gotoxy(poz_x,poz_y); //72 vniz 80 verh
-                    cout << char(42);
-                    gotoxy(poz_x,poz_y+1);
-                    cout << " ";
+            gotoxy(poz_x,poz_y); //72 vniz 80 verh
+            cout << char(42);
+            gotoxy(poz_x,poz_y+1);
+            cout << " ";
         }
 
-        else if (Keys == 80 )
-        {
+        else if (Keys == 80 ) {
             poz_y+=1;
-                    gotoxy(poz_x,poz_y); //72 vniz 80 verh
-                    cout << char(42);
-                    gotoxy(poz_x,poz_y-1);
-                    cout << " ";
+            gotoxy(poz_x,poz_y); //72 vniz 80 verh
+            cout << char(42);
+            gotoxy(poz_x,poz_y-1);
+            cout << " ";
         }
-
     }
 
     cin.get();
